98652e3065f0fa089f0f0f8d029be1205c6faeb0
Ove Nystås <ove.nystas@gmail.com>
2018-10-17 23:16:41 +0200

    First version with DHT22 support

--- houseVentFanCtrl.ino
+++ houseVentFanCtrl.ino
@@ -9,13 +9,21 @@
  * Five out of six relays on a six relay module board are used.
  * Speed is set remotely via Z-Wave protocol using a Z-Uno Arduino board.
  * Last set speed is remembered and used to reset relays to same state after a power outage.
+ * 
+ * There is also a DHT22 sensor on board which provides a measurement of the relative humidity and
+ * the temperature.
  */
 
 #include "EEPROM.h"
+#include "ZUNO_DHT.h"
 
 
 #define NUMBER_OF_RELAYS 6
 #define EEPROM_ADDRESS 0x0000
+
+#define TEMP_HYSTERESIS 10  // In 10th of degC
+#define HUM_HYSTERESIS 10  // In 10th of %
+#define READ_INTERVAL_MS 30000  // Read every 30 seconds
 
 
 // Pin numbers, where the six relays are connected (avoiding standard LED pin)
@@ -33,10 +41,43 @@ const uint8_t relayTable[6][5] =
   { HIGH, HIGH, LOW,  LOW,  LOW  },  // Full speed
 };
 
+DHT dht22(16, DHT22);
+int16_t temperature = 0;      // In 10th of Celsius.
+int16_t sentTemperature = 0;  // In 10th of Celsius.
+int16_t humidity = 0;         // In 10th of percent.
+int16_t sentHumidity = 0;     // In 10th of percent.
+
+
+ZUNO_SETUP_DEBUG_MODE(DEBUG_ON);
 
 // Setup the Z-Wave channel for Z-Uno.
-ZUNO_SETUP_CHANNELS(ZUNO_SWITCH_MULTILEVEL(getter, setter));
+ZUNO_SETUP_CHANNELS(
+  ZUNO_SWITCH_MULTILEVEL(
+    getterFan,
+    setterFan
+  ),
+  ZUNO_SENSOR_MULTILEVEL(
+    ZUNO_SENSOR_MULTILEVEL_TYPE_TEMPERATURE,
+    SENSOR_MULTILEVEL_SCALE_CELSIUS,
+    SENSOR_MULTILEVEL_SIZE_TWO_BYTES,
+    SENSOR_MULTILEVEL_PRECISION_ONE_DECIMAL,
+    getterTemperature
+  ),
+  ZUNO_SENSOR_MULTILEVEL(
+    ZUNO_SENSOR_MULTILEVEL_TYPE_RELATIVE_HUMIDITY,
+    SENSOR_MULTILEVEL_SCALE_PERCENTAGE_VALUE,
+    SENSOR_MULTILEVEL_SIZE_TWO_BYTES,
+    SENSOR_MULTILEVEL_PRECISION_ONE_DECIMAL,
+    getterHumidity
+  )
+);
 
+typedef enum
+{
+  CH_FAN = 1,
+  CH_TEMP,
+  CH_HUM
+} channel_type_t;
 
 void setup(void)
 {
@@ -48,13 +89,37 @@ void setup(void)
   }
 
   // Retreive last set value from EEPROM and reset relays according to it.
-  setter(EEPROM.read(EEPROM_ADDRESS));
+  setterFan(EEPROM.read(EEPROM_ADDRESS));
+
+  dht22.begin();
 }
 
 
 void loop(void)
 {
   // Empty. Functions getter() and setter() are called from Z-Uno.
+  static uint32_t lastMillis = 0;
+  
+  uint32_t currentMillis = millis();
+  if (currentMillis - lastMillis >= READ_INTERVAL_MS)
+  {
+    lastMillis = currentMillis;
+     
+    temperature = dht22.readTemperatureC10();
+    humidity = dht22.readHumidityH10();
+
+    if (abs(temperature - sentTemperature) >= TEMP_HYSTERESIS)  // >= 1.0 degC diff
+    {
+      zunoSendReport(CH_TEMP);
+      sentTemperature = temperature;
+    }
+
+    if (abs(humidity - sentHumidity) >= HUM_HYSTERESIS)  // >= 1.0 % diff
+    {
+      zunoSendReport(CH_HUM);
+      sentHumidity = humidity;
+    }
+  }
 }
 
 
@@ -90,18 +155,30 @@ uint8_t convertSpeed(uint8_t percentage)
 
 
 // Gets the current speed percentage value.
-uint8_t getter(void)
+uint8_t getterFan(void)
 {
   return EEPROM.read(EEPROM_ADDRESS);
 }
 
 
 // Sets new relay state depending on new speed percentage value of 0-99%.
-void setter(uint8_t newValue)
+void setterFan(uint8_t newValue)
 {
   // Set the relays according to new speed value.
   setSpeedRelays(convertSpeed(newValue));
     
   // Save the new value in EEPROM to survive power outage.
   EEPROM.update(EEPROM_ADDRESS, newValue);
+}
+
+
+uint16_t getterTemperature()
+{
+  return temperature;
+}
+
+
+uint16_t getterHumidity()
+{
+  return humidity;
 }
